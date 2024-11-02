014065890424500c95364f2a01dfe12a81ed29ee
Amoyzzz <dharkarajveer@gmail.com>
2024-11-02 00:14:45 -0400

    Working model, however it's a little bit off especially with shadows and stuff

--- Model/testmodel.py
+++ Model/testmodel.py
@@ -1,15 +1,21 @@
 import os
 from ultralytics import YOLO
 
-model = YOLO("C:\Users\meggs\runs\detect\train9\weights\best.pt")
+model = YOLO(r'C:\Users\meggs\runs\detect\train9\weights\best.pt')
 
-image_dir = r'Ping Pong Detection.v3i.yolov\test\images' #Raw String
-output_dir = r'Ping Pong Detection.v3i.yolov11\test\outputs' #r' = raw string
+image_dir = r'C:\Users\meggs\OneDrive\Documents\GitHub\DroneVision\Ping Pong Detection.v3i.yolov11\test\images' #Raw String
+output_dir = r'C:\Users\meggs\OneDrive\Documents\GitHub\DroneVision\Ping Pong Detection.v3i.yolov11\test\outputs' #r' = raw string
 
 os.makedirs(output_dir, exist_ok=True)
 
+max_count = 5
+count = 0
+
 for filename in os.listdir(image_dir):
-    if filename.lower().endswith(('jpg', '.jpeg', '.png')):
+    if filename.lower().endswith(('jpg', '.jpeg', '.png')):        
+        if count > max_count:
+            break
+
         image_path = os.path.join(image_dir, filename)
 
         results = model(image_path)
@@ -25,3 +31,5 @@ for filename in os.listdir(image_dir):
 
             output_path = os.path.join(output_dir, filename)
             result.save(filename=output_path)
+        count += 1
+
