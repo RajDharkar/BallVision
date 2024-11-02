8dcf2187237ab5c17c0e3bc7cd3da5bc7867bc07
Amoyzzz <dharkarajveer@gmail.com>
2024-11-02 00:23:18 -0400

    well it works now, but sometimes it messes up

--- Model/testmodel.py
+++ Model/testmodel.py
@@ -4,12 +4,12 @@ from ultralytics import YOLO
 model = YOLO(r'C:\Users\meggs\runs\detect\train9\weights\best.pt')
 
 image_dir = r'C:\Users\meggs\OneDrive\Documents\GitHub\DroneVision\Ping Pong Detection.v3i.yolov11\test\images' #Raw String
-output_dir = r'C:\Users\meggs\OneDrive\Documents\GitHub\DroneVision\Ping Pong Detection.v3i.yolov11\test\outputs' #r' = raw string
+output_dir = r'Ping Pong Detection.v3i.yolov11\test\outputs' #r' = raw string
 
 os.makedirs(output_dir, exist_ok=True)
 
-max_count = 5
 count = 0
+max_count = 50
 
 for filename in os.listdir(image_dir):
     if filename.lower().endswith(('jpg', '.jpeg', '.png')):        
@@ -26,8 +26,6 @@ for filename in os.listdir(image_dir):
             keypoints = result.keypoints
             probs = result.probs
             obs = result.obb
-
-            result.show()
 
             output_path = os.path.join(output_dir, filename)
             result.save(filename=output_path)
